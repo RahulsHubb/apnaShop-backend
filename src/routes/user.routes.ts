d3aad21cc59873a50c160b165436987db98e67df
Rahul Dewal <60481248+RahulsHubb@users.noreply.github.com>
2025-12-30 18:03:53 +0530

    Fix formatting and update route definitions

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,9 @@
-import express from 'express'
-import { createUser, getUserByEmail } from '../controllers/getUserByEmail.js'
+import express from "express";
+import { createUser, getUserByEmail } from "../controllers/getUserByEmail.js";
 
-const router = express.Router()
+const router = express.Router();
 
+router.post("/users/register", createUser);
+router.get("/users/:email", getUserByEmail);
 
-router.post('/users', createUser)
-router.get('/users/:email', getUserByEmail)
-
-export default router
+export default router;
