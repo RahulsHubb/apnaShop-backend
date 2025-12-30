083ffffae26abf77f2099cdb322dfd8a8bda123d
Rahul Dewal <60481248+RahulsHubb@users.noreply.github.com>
2025-12-30 18:07:28 +0530

    Fix import path for User model in profile.controller

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { User } from "../model/user.model.js";
+import { User } from "../models/user.model.js";
 
 export const getProfile = (req: Request, res: Response) => {
   res.status(200).json({
