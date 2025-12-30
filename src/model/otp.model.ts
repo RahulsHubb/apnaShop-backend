b9e938be06dab40a03f8abce4348cd41c1e78cb3
Rahul Dewal <60481248+RahulsHubb@users.noreply.github.com>
2025-12-30 18:06:52 +0530

    Create OTP model with schema and export

---
+++ src/model/otp.model.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema({
+  phone: { type: String, required: true, index: true },
+  otp: { type: String, required: true },
+  expiresAt: { type: Date, required: true, index: { expires: 0 } },
+});
+
+export default mongoose.model("otp", otpSchema);
